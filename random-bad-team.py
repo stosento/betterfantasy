f54900aef9a5b5e49c8223730fb1d0e70a7361e2
Stephen Osentoski <stosento@umich.edu>
2024-05-04 21:42:35 -0400

    Some clean-up & added comments

--- random-bad-team.py
+++ random-bad-team.py
@@ -4,7 +4,7 @@ import cfbd
 from datetime import datetime
 import random
 from tqdm import tqdm
-from utils import TwilioTexter
+# from utils import TwilioTexter
 from constants import ESPN_FPI_URL, CFB_REFERENCE_NAME_EXCEPTIONS, ACCEPTABLE_CONFERENCES, FANTASY_TEAMS, CFBD_API_KEY
 
 # Configure API key authorization: ApiKeyAuth
@@ -12,10 +12,14 @@ configuration = cfbd.Configuration()
 configuration.api_key['Authorization'] = CFBD_API_KEY
 configuration.api_key_prefix['Authorization'] = 'Bearer'
 
+# Read in FPI data
 fpi_rnk_df = pd.read_html(ESPN_FPI_URL)[0]
 
-BOTTOM_N = len(FANTASY_TEAMS)
-twilio_texter = TwilioTexter()
+NUM_FANTASY_TEAMS = len(FANTASY_TEAMS)
+# twilio_texter = TwilioTexter()
+
+test_date = datetime.strptime('2024-08-28', '%Y-%m-%d').date()
+print('Test date: ', test_date)
 
 def get_date(date_str):
     return pd.to_datetime(date_str).date()
@@ -25,30 +29,34 @@ def get_conference_teams(ACCEPTABLE_CONFERENCES=ACCEPTABLE_CONFERENCES, EXCEPTIO
     api_instance = cfbd.TeamsApi(cfbd.ApiClient(configuration))
     teams = api_instance.get_fbs_teams(year=datetime.today().year)
 
-    keep_teams = []
+    valid_teams = []
     for team in teams:
         team_name = team.school
         team_conference = team.conference
         
         if team_conference in ACCEPTABLE_CONFERENCES:
-            keep_teams.append(team_name)
+            valid_teams.append(team_name)
 
-    return keep_teams
+    return valid_teams
 
 
-def get_team_schedule(team_name):
-    todays_date = datetime.today().date()
+def get_team_schedule(team_name, date):
+    todays_date = date
+
     api_instance = cfbd.GamesApi(cfbd.ApiClient(configuration))
     team_schedule = api_instance.get_games(year=todays_date.year, team=team_name)
+
     return team_schedule
 
 
-def get_next_game(team_name):
-    todays_date = datetime.today().date()
-    team_schedule = get_team_schedule(team_name)
+def get_next_game(team_name, date): # TODO - Update with param for accepting a date
+    todays_date = date
+
+    team_schedule = get_team_schedule(team_name, todays_date)
 
     game_dates = [get_date(game.start_date) for game in team_schedule]
     next_game_date = min(filter(lambda x: x >= todays_date, game_dates))
+
     # get first (and only) element from iterator
     next_game = list(filter(lambda x: get_date(x.start_date) == next_game_date, team_schedule))
     if len(next_game) == 0:
@@ -59,16 +67,19 @@ def get_next_game(team_name):
     return next_game
 
 
-def team_has_game_this_week(team_name):
-    todays_date = datetime.today().date()
-    team_next_game = get_next_game(team_name)
+def team_has_game_this_week(team_name, date):
+    #todays_date = datetime.today().date() # TODO - Update with param for accepting a date
+    todays_date = date
+
+    team_next_game = get_next_game(team_name, todays_date)
+
     team_next_game_date = get_date(team_next_game.start_date)
     return (team_next_game_date - todays_date).days <= 5  # set at 5 to allow for weird day of week games 
 
 
 def get_team_fpi_rating(team_name):
     api_instance = cfbd.RatingsApi(cfbd.ApiClient(configuration))
-    api_response = api_instance.get_fpi_ratings(year=datetime.today().year, team=team_name)
+    api_response = api_instance.get_fpi_ratings(year='2023', team=team_name) #TODO - Update with param for accepting a year
     try:
         fpi = api_response[0].fpi
         return fpi
@@ -87,77 +98,67 @@ def get_team_record(team_name):
     return record_str
 
 
-def get_bottom_n_teams(fpi_dict, bottom_n=len(FANTASY_TEAMS)):
-    worst_n_scores = []
-    for k, v in fpi_dict.items():
-        if len(worst_n_scores) < bottom_n:
-            worst_n_scores.append(v)
-        else:
-            if v < max(worst_n_scores):
-                worst_n_scores.remove(max(worst_n_scores))
-                worst_n_scores.append(v)
-    
-    worst_teams = [k for k, v in fpi_dict.items() if v in worst_n_scores]
-    return worst_teams[0:bottom_n]
+def get_bottom_n_teams(fpi_dict, bottom_n=NUM_FANTASY_TEAMS):
+    sorted_dict = dict(sorted(fpi_dict.items(), key=lambda item: item[1]))
+    return list(sorted_dict.keys())[0:bottom_n]
 
 
 def format_game_info_for_text(fantasy_team, team, game, team_record):
     team_info = f'{team} ({team_record})'
 
     is_home = game.home_team == team
-
-    if is_home:
-        opponent_info = game.away_team
-    else:
-        opponent_info = game.home_team
-
-    if not is_home:
-        body = f'{team_info}, playing @ {opponent_info}'
-    else:
-        body = f'{team_info}, home vs {opponent_info}'
+    opponent_info = game.away_team if is_home else game.home_team
+    body = f'{team_info}, {"home vs" if is_home else "playing @"} {opponent_info}'
 
     message = f'{fantasy_team} has {body}'
     return message
 
+def main():
+    # Retrieve valid teams within our acceptable conferences
+    all_teams = get_conference_teams()
+    print('Got teams.')
 
+    # Building map of {team -> FPI Ranking} for those teams that have a game in the upcoming week
+    print('Getting schedules.')
+    keep_teams_fpi = {}
+    for team in tqdm(all_teams):
+        sleep(0.5)
+        try:
+            has_game = team_has_game_this_week(team, test_date)
+            if has_game:
+                api_instance = cfbd.RatingsApi(cfbd.ApiClient(configuration))
 
-all_teams = get_conference_teams()
-print('Got teams.')
+                keep_teams_fpi[team] = get_team_fpi_rating(team)
+        except:
+            print("Couldn't get schedule for: {}".format(team))
+        
+    print('Finished cfb data part!')
 
-print('Getting schedules.')
-keep_teams_fpi = {}
-for team in tqdm(all_teams):
-    sleep(0.5)
-    try:
-        has_game = team_has_game_this_week(team)
-        if has_game:
-            api_instance = cfbd.RatingsApi(cfbd.ApiClient(configuration))
+    # Extract the worst N teams from our map
+    bottom_teams = get_bottom_n_teams(keep_teams_fpi)
 
-            keep_teams_fpi[team] = get_team_fpi_rating(team)
-    except:
-        print("Couldn't get schedule for: {}".format(team))
-    
-print('Finished cfb data part!')
+    # Shuffle each list
+    random.shuffle(FANTASY_TEAMS)
+    random.shuffle(bottom_teams)
 
-bottom_teams = get_bottom_n_teams(keep_teams_fpi)
+    # Create a tuple for each fantasy team and the team they will be assigned
+    pairs = {}
+    for team, fantasy_team in zip(bottom_teams, FANTASY_TEAMS):
+        pairs[fantasy_team] = team
 
-random.shuffle(FANTASY_TEAMS)
-random.shuffle(bottom_teams)
+    text_lines = []
+    for fantasy_team, team in pairs.items():
+        next_game = get_next_game(team, test_date)
+        team_record = get_team_record(team)
 
-pairs = {}
-for team, fantasy_team in zip(bottom_teams, FANTASY_TEAMS):
-    pairs[fantasy_team] = team
+        text_body = format_game_info_for_text(fantasy_team, team, next_game, team_record)
+        text_lines.append(text_body)
 
-text_lines = []
-for fantasy_team, team in pairs.items():
-    next_game = get_next_game(team)
-    team_record = get_team_record(team)
+    full_text_body = '\n'.join(text_lines)
 
-    text_body = format_game_info_for_text(fantasy_team, team, next_game, team_record)
-    text_lines.append(text_body)
+    print('Full text body: ', full_text_body)
 
-full_text_body = '\n'.join(text_lines)
+    # twilio_texter.send_text(to_number='+17346523203', body=full_text_body)
 
-print(full_text_body)
-
-twilio_texter.send_text(to_number='+17346523203', body=full_text_body)
+if __name__ == '__main__':
+    main()

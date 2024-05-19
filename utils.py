292de971991acc77e0c2f1f1e2287c82b85590c5
Stephen Osentoski <stosento@umich.edu>
2024-05-18 22:00:38 -0400

    Remove secrets from code

--- utils.py
+++ utils.py
@@ -1,14 +1,18 @@
 from twilio.rest import Client
+from dotenv import load_dotenv
 
 class TwilioTexter():
     def __init__(self):
         self.client = self.get_twilio_client()
     
-
     def get_twilio_client(self):
-        aid = 'ACbcf8d51a96ed0013d121e1e47330f0c6'
-        a_token = 'e3b933ce7a5553e2775bf3e5113f908c'
+
+        load_dotenv()
+
+        aid = os.getenv('TWILIO_AID')
+        a_token = os.getenv('TWILIO_TOKEN')
         client = Client(aid, a_token)
+
         return client
 
     def send_text(self, to_number, body, from_number="+18554216531"):

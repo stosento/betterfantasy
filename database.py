e703a0f918ac3555cc32293bd5913fca42837bac
Stephen Osentoski <stosento@umich.edu>
2024-07-16 11:18:38 -0400

    Fixing deployment commands

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, __version__ as sqlalchemy_version
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.dialects import postgresql
